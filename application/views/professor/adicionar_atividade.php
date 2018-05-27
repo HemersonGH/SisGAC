4709f74805f1998d252c1f8a70eb73860dc7eaab
Hemerson-GH <hemersonel@gmail.com>
2018-05-27 02:48:42 -0300

    adicionando compontes iteração, not definede

--- application/views/professor/adicionar_atividade.php
+++ application/views/professor/adicionar_atividade.php
@@ -1,36 +1,26 @@
-<link href="<?= base_url(); ?>assets/css/disciplina_professor.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/css/disciplina_professor.css" rel="stylesheet">
 
-<main class="row col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3 main">
-  <div class="col-md-10">
-    <h1 class="page-header"> <?= $disciplina[0]->nome_disciplina ?> </h1>
-  </div>
-
-  <div class="col-md-2">
-    <a class="btn btn-primary btn-block" href="<?= base_url(); ?>professor/criar_iteracao"> Adicionar Iteração </a>
-  </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="myModalE" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4> Excluir Disciplina </h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+    <main class="row col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3 main">
+      <div class="col-md-10">
+        <h1 class="page-header"> <?= $disciplina[0]->nome_disciplina ?> </h1>
       </div>
-      <div class="modal-body">
-        Tem certeza que deseja excluir essa disciplina?
-      </div>
-      <div class="modal-footer">
-        <a type="button" class="btn btn-danger" style="cursor:pointer; color: white;" data-dismiss="modal"> Não </a>
-        <a type="button" class="btn btn-primary" href="<?= base_url('professor/excluir_disciplina/'.$disciplina->idDisciplina); ?>"> Sim </a>
-      </div>
-    </div>
-  </div>
-</div>
 
-</main>
-</div>
+      <div class="col-md-2">
+        <a class="btn btn-primary btn-block" href="<?= base_url('professor/criar_iteracao/'.$disciplina[0]->idDisciplina); ?>"> Adicionar Iteração </a>
+      </div>
+
+      <div class="padding col-md-12">
+        <h4> Minhas Disciplinas </h4>
+        <table class="table table-striped">
+          <tr>
+            <th> Número da Iteração </th>
+            <th> Código da Disciplina </th>
+            <th> Disciplina </th>
+            <th> Ações </th>
+          </tr>
+        </table>
+
+      </div>
+    </main>
+  </div>
 </div>
