0fa190bc5df14419b82b1d881aea18e31f71d75b
Hemerson-GH <hemersonel@gmail.com>
2018-06-06 18:13:49 -0300

    alterando bd

--- system/database/backup/db_sgd.sql
+++ system/database/backup/db_sgd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2018 at 03:58 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.1.16
+-- Generation Time: 06-Jun-2018 às 23:13
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `atividade`
+-- Estrutura da tabela `atividade`
 --
 
 CREATE TABLE `atividade` (
@@ -39,21 +39,16 @@ CREATE TABLE `atividade` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `atividade`
+-- Extraindo dados da tabela `atividade`
 --
 
 INSERT INTO `atividade` (`idAtividade`, `nome_atividade`, `descricao_atividade`, `prazo_entrega`, `idConjuntoAtividade`, `idProfessor`, `pontos`) VALUES
-(16, 'Descrever modelo de ciclo de vida', 'Descreva o modelo de ciclo de vida adotado para o desenvolvimento do software', '2018-07-25 03:00:00', 18, 7, '7'),
-(17, 'Definir a o escopo do projeto', 'Definir a o escopo do projeto (objetivo, principais funcionalidades e restrições)', '2018-06-10 03:00:00', 18, 7, '5'),
-(18, 'Selecionar ferramenta de Gerência', 'Selecione e adote uma ferramenta para gerenciar as atividades de desenvolvimento e defina o backlog do produto nesta ferramenta', '2018-07-25 03:00:00', 18, 7, '5'),
-(19, 'Criar projeto no Git-Hub', 'Criar projeto no Git-Hub e adicionar todos os envolvidos', '2018-04-19 03:00:00', 19, 7, '3'),
-(20, 'Definir e seguir políticas de uso do Git', 'Definir e seguir políticas de uso do Git no leia-me do projeto (mínimo: estrutura de diretórios e quais tipos de produtos de trabalho serão armazenados em cada\r\ndiretório).', '2018-07-19 03:00:00', 19, 7, '7'),
-(21, 'sdgewrwer', '5235ewrwrewrewrewrew', '2018-06-05 03:00:00', 24, 7, '3');
+(21, 'elicitação de requisitos', 'lorem ipsum', '2018-06-06 03:00:00', 25, 8, '3');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `conjunto_atividade`
+-- Estrutura da tabela `conjunto_atividade`
 --
 
 CREATE TABLE `conjunto_atividade` (
@@ -64,19 +59,16 @@ CREATE TABLE `conjunto_atividade` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `conjunto_atividade`
+-- Extraindo dados da tabela `conjunto_atividade`
 --
 
 INSERT INTO `conjunto_atividade` (`idConjuntoAtividade`, `nome_conjunto`, `idProfessor`, `idDisciplina`) VALUES
-(18, 'Gerência de Projetos', 7, 10),
-(19, 'Gerência de Configuração', 7, 10),
-(22, 'link park', 7, 13),
-(24, 'cabrititinha', 7, NULL);
+(25, 'gerencia de requisitos', 8, 15);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `disciplina`
+-- Estrutura da tabela `disciplina`
 --
 
 CREATE TABLE `disciplina` (
@@ -89,20 +81,16 @@ CREATE TABLE `disciplina` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `disciplina`
+-- Extraindo dados da tabela `disciplina`
 --
 
 INSERT INTO `disciplina` (`idDisciplina`, `idProfessor`, `nome_disciplina`, `codigo_disciplina`, `descricao_disciplina`, `status_disciplina`) VALUES
-(10, 7, 'Engenharia de Software', 'GCC-112', 'Engenharia de software é uma área da computação voltada à especificação, desenvolvimento, manutenção e criação de software, com a aplicação de tecnologias e práticas de gerência de projetos.', 2),
-(11, 7, 'Banco de Dados', 'GCC-143', 'Bancos de dados ou bases de dados são um conjunto de arquivos relacionados entre si com registros sobre pessoas, lugares ou coisas.', 1),
-(12, 7, 'Algoritmos em Grafos', 'GCC-172', 'Grafos', 3),
-(13, 7, 'dsfdfdsf457', 'dsfsd', 'sdf547547', 1),
-(15, 7, 'fjgfj', 'gfj', 'gfjgfj', 1);
+(15, 8, 'sw', '123', 'legal', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `participa_disciplina`
+-- Estrutura da tabela `participa_disciplina`
 --
 
 CREATE TABLE `participa_disciplina` (
@@ -113,17 +101,16 @@ CREATE TABLE `participa_disciplina` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `participa_disciplina`
+-- Extraindo dados da tabela `participa_disciplina`
 --
 
 INSERT INTO `participa_disciplina` (`idAluno`, `idDisciplina`, `status_participacao`, `idProfessor`) VALUES
-(6, 10, 1, 7),
-(6, 12, 1, 7);
+(9, 15, 1, 8);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `realiza_atividade`
+-- Estrutura da tabela `realiza_atividade`
 --
 
 CREATE TABLE `realiza_atividade` (
@@ -138,19 +125,16 @@ CREATE TABLE `realiza_atividade` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `realiza_atividade`
+-- Extraindo dados da tabela `realiza_atividade`
 --
 
 INSERT INTO `realiza_atividade` (`idAtividade`, `idAluno`, `idDisciplina`, `idProfessor`, `resposta_aluno`, `anexo`, `status_avaliacao`, `resposta_professor`) VALUES
-(16, 6, 10, 7, 'ff', 'Hemerson_Batista_Filho_6_16.docx', 3, 'dsfdsfdsf'),
-(17, 6, 10, 7, 'kjsdfksdn sdfdskfjdsf sdfksdjfkds fdskfjsdkfsd', 'Hemerson_Batista_Filho_6_17.pdf', 3, 'fdgfdg'),
-(18, 6, 10, 7, 'sfdsfdsf', 'Hemerson_Batista_Filho_6_18.docx', 2, NULL),
-(19, 6, 10, 7, 'hjgfhjfg', 'Hemerson_Batista_Filho_6_19.docx', 2, NULL);
+(21, 9, 15, 8, 'evidencia', 'aluno_9_21.docx', 3, 'errado.. falta tal coisa');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `solicitacoes_disciplinas`
+-- Estrutura da tabela `solicitacoes_disciplinas`
 --
 
 CREATE TABLE `solicitacoes_disciplinas` (
@@ -164,21 +148,16 @@ CREATE TABLE `solicitacoes_disciplinas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `solicitacoes_disciplinas`
+-- Extraindo dados da tabela `solicitacoes_disciplinas`
 --
 
 INSERT INTO `solicitacoes_disciplinas` (`idSolicitacao`, `idAluno`, `idDisciplina`, `justificativa_aluno`, `status_solicitacao`, `idProfessor`, `justificativa_professor`) VALUES
-(7, 6, 10, 'Desejo me matrícular nessa disciplna afim de realizar as demais atividades oferecidas pela mesma.', 2, 7, 'Aceito sua solicitação.'),
-(8, 6, 12, 'Desejo me matrícular nessa disciplna afim de realizar as demais atividades oferecidas pela mesma.', 2, 7, 'Aceito'),
-(9, 6, 10, 'dgsgsdgs', 2, 7, 'cfhdfhdfhd'),
-(10, 6, 10, 'fsdfdsfsdfsd', 1, 7, ''),
-(11, 6, 10, 't', 1, 7, ''),
-(12, 6, 10, 'retret', 1, 7, '');
+(11, 9, 15, 'oi', 2, 8, 'ok');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `trofeus`
+-- Estrutura da tabela `trofeus`
 --
 
 CREATE TABLE `trofeus` (
@@ -187,7 +166,7 @@ CREATE TABLE `trofeus` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `trofeus`
+-- Extraindo dados da tabela `trofeus`
 --
 
 INSERT INTO `trofeus` (`idTrofeu`, `nome_trofeu`) VALUES
@@ -198,7 +177,7 @@ INSERT INTO `trofeus` (`idTrofeu`, `nome_trofeu`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `trofeus_atividades`
+-- Estrutura da tabela `trofeus_atividades`
 --
 
 CREATE TABLE `trofeus_atividades` (
@@ -209,27 +188,26 @@ CREATE TABLE `trofeus_atividades` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuario`
+-- Estrutura da tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
   `idUsuario` int(11) NOT NULL,
   `nome` varchar(80) NOT NULL,
   `cpf` varchar(80) NOT NULL,
-  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `data` date NOT NULL,
   `email` varchar(80) NOT NULL,
   `senha` varchar(80) NOT NULL,
   `tipoUsuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `usuario`
+-- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nome`, `cpf`, `data`, `email`, `senha`, `tipoUsuario`) VALUES
-(6, 'Hemerson Batista Filho', 'XXX.XXX.XXX-XX', '2018-06-05 13:41:57', 'aluno@aluno', 'ca0cd09a12abade3bf0777574d9f987f', 1),
-(7, 'Antônio Carlos Silva', 'XXX.XXX.XXX-XX', '1988-09-21 03:00:00', 'professor@professor', 'd450c5dbcc10db0749277efc32f15f9f', 2),
-(8, 'e456346', '346346', '2018-06-05 03:00:00', '34634@dsgds', 'c0a62e133894cdce435bcb4a5df1db2d', 2);
+(10, 'dsffsdf', 'dsfsd', '2018-06-06', 'emai@a', 'd4b2758da0205c1e0aa9512cd188002a', 2),
+(11, 'aa', 'aa', '2018-06-22', 'aaa@aaa', '4124bc0a9335c27f086f24ba207a4912', 1);
 
 --
 -- Indexes for dumped tables
@@ -297,7 +275,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `atividade`
 --
 ALTER TABLE `atividade`
-  MODIFY `idAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `idAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `conjunto_atividade`
@@ -315,7 +293,7 @@ ALTER TABLE `disciplina`
 -- AUTO_INCREMENT for table `solicitacoes_disciplinas`
 --
 ALTER TABLE `solicitacoes_disciplinas`
-  MODIFY `idSolicitacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idSolicitacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `trofeus`
@@ -327,7 +305,7 @@ ALTER TABLE `trofeus`
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
