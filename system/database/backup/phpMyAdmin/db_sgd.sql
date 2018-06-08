f01e08d0257820d81210814fbdbc3e66b5cc53a8
hemerson.batista <hemersonbatista.gt4w@gmail.com>
2018-06-08 08:48:31 -0300

    criando backup bd phpMyAdmin

--- system/database/backup/phpMyAdmin/db_sgd.sql
+++ system/database/backup/phpMyAdmin/db_sgd.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 06-Jun-2018 às 23:13
+-- Generation Time: 08-Jun-2018 às 13:38
 -- Versão do servidor: 10.1.31-MariaDB
--- PHP Version: 7.2.4
+-- PHP Version: 7.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,18 +32,18 @@ CREATE TABLE `atividade` (
   `idAtividade` int(11) NOT NULL,
   `nome_atividade` varchar(100) NOT NULL,
   `descricao_atividade` varchar(200) NOT NULL,
-  `prazo_entrega` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `prazo_entrega` date NOT NULL,
   `idConjuntoAtividade` int(11) NOT NULL,
   `idProfessor` int(11) NOT NULL,
   `pontos` decimal(11,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `atividade`
 --
 
 INSERT INTO `atividade` (`idAtividade`, `nome_atividade`, `descricao_atividade`, `prazo_entrega`, `idConjuntoAtividade`, `idProfessor`, `pontos`) VALUES
-(21, 'elicitação de requisitos', 'lorem ipsum', '2018-06-06 03:00:00', 25, 8, '3');
+(3, '52', 'dsf', '2018-06-08', 2, 2, '43');
 
 -- --------------------------------------------------------
 
@@ -53,17 +53,17 @@ INSERT INTO `atividade` (`idAtividade`, `nome_atividade`, `descricao_atividade`,
 
 CREATE TABLE `conjunto_atividade` (
   `idConjuntoAtividade` int(11) NOT NULL,
-  `nome_conjunto` varchar(80) NOT NULL,
+  `nome_conjunto` varchar(45) NOT NULL,
   `idProfessor` int(11) NOT NULL,
   `idDisciplina` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `conjunto_atividade`
 --
 
 INSERT INTO `conjunto_atividade` (`idConjuntoAtividade`, `nome_conjunto`, `idProfessor`, `idDisciplina`) VALUES
-(25, 'gerencia de requisitos', 8, 15);
+(2, 'xfx', 2, NULL);
 
 -- --------------------------------------------------------
 
@@ -75,17 +75,10 @@ CREATE TABLE `disciplina` (
   `idDisciplina` int(11) NOT NULL,
   `idProfessor` int(11) NOT NULL,
   `nome_disciplina` varchar(80) NOT NULL,
-  `codigo_disciplina` varchar(10) NOT NULL,
+  `codigo_disciplina` varchar(20) NOT NULL,
   `descricao_disciplina` varchar(200) NOT NULL,
-  `status_disciplina` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `disciplina`
---
-
-INSERT INTO `disciplina` (`idDisciplina`, `idProfessor`, `nome_disciplina`, `codigo_disciplina`, `descricao_disciplina`, `status_disciplina`) VALUES
-(15, 8, 'sw', '123', 'legal', 2);
+  `status_disciplina` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -96,16 +89,9 @@ INSERT INTO `disciplina` (`idDisciplina`, `idProfessor`, `nome_disciplina`, `cod
 CREATE TABLE `participa_disciplina` (
   `idAluno` int(11) NOT NULL,
   `idDisciplina` int(11) NOT NULL,
-  `status_participacao` tinyint(1) NOT NULL,
+  `status_participacao` int(11) NOT NULL,
   `idProfessor` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `participa_disciplina`
---
-
-INSERT INTO `participa_disciplina` (`idAluno`, `idDisciplina`, `status_participacao`, `idProfessor`) VALUES
-(9, 15, 1, 8);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -114,76 +100,31 @@ INSERT INTO `participa_disciplina` (`idAluno`, `idDisciplina`, `status_participa
 --
 
 CREATE TABLE `realiza_atividade` (
-  `idAtividade` int(11) NOT NULL,
   `idAluno` int(11) NOT NULL,
-  `idDisciplina` int(11) NOT NULL,
-  `idProfessor` int(11) NOT NULL,
-  `resposta_aluno` varchar(300) DEFAULT NULL,
+  `idAtividade` int(11) NOT NULL,
+  `idDisciplina` varchar(45) NOT NULL,
+  `idProfessor` varchar(45) NOT NULL,
+  `resposta_aluno` varchar(200) NOT NULL,
   `anexo` varchar(200) NOT NULL,
   `status_avaliacao` int(11) NOT NULL DEFAULT '1',
-  `resposta_professor` varchar(300) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `realiza_atividade`
---
-
-INSERT INTO `realiza_atividade` (`idAtividade`, `idAluno`, `idDisciplina`, `idProfessor`, `resposta_aluno`, `anexo`, `status_avaliacao`, `resposta_professor`) VALUES
-(21, 9, 15, 8, 'evidencia', 'aluno_9_21.docx', 3, 'errado.. falta tal coisa');
+  `resposta_professor` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `solicitacoes_disciplinas`
+-- Estrutura da tabela `solicitacao_disciplina`
 --
 
-CREATE TABLE `solicitacoes_disciplinas` (
-  `idSolicitacao` int(11) NOT NULL,
+CREATE TABLE `solicitacao_disciplina` (
   `idAluno` int(11) NOT NULL,
   `idDisciplina` int(11) NOT NULL,
-  `justificativa_aluno` varchar(250) NOT NULL,
-  `status_solicitacao` int(11) NOT NULL DEFAULT '1',
   `idProfessor` int(11) NOT NULL,
-  `justificativa_professor` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `solicitacoes_disciplinas`
---
-
-INSERT INTO `solicitacoes_disciplinas` (`idSolicitacao`, `idAluno`, `idDisciplina`, `justificativa_aluno`, `status_solicitacao`, `idProfessor`, `justificativa_professor`) VALUES
-(11, 9, 15, 'oi', 2, 8, 'ok');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `trofeus`
---
-
-CREATE TABLE `trofeus` (
-  `idTrofeu` int(11) NOT NULL,
-  `nome_trofeu` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `trofeus`
---
-
-INSERT INTO `trofeus` (`idTrofeu`, `nome_trofeu`) VALUES
-(1, 'Ouro'),
-(2, 'Prata'),
-(3, 'Bronze');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `trofeus_atividades`
---
-
-CREATE TABLE `trofeus_atividades` (
-  `idAtividade` int(11) NOT NULL,
-  `idTrofeu` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status_solicitacao` int(11) NOT NULL,
+  `justificativa_aluno` varchar(250) NOT NULL,
+  `justificativa_professor` varchar(250) NOT NULL,
+  `idSolicitacao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -194,20 +135,20 @@ CREATE TABLE `trofeus_atividades` (
 CREATE TABLE `usuario` (
   `idUsuario` int(11) NOT NULL,
   `nome` varchar(80) NOT NULL,
-  `cpf` varchar(80) NOT NULL,
+  `cpf` varchar(20) NOT NULL,
   `data` date NOT NULL,
   `email` varchar(80) NOT NULL,
-  `senha` varchar(80) NOT NULL,
+  `senha` varchar(150) NOT NULL,
   `tipoUsuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`idUsuario`, `nome`, `cpf`, `data`, `email`, `senha`, `tipoUsuario`) VALUES
-(10, 'dsffsdf', 'dsfsd', '2018-06-06', 'emai@a', 'd4b2758da0205c1e0aa9512cd188002a', 2),
-(11, 'aa', 'aa', '2018-06-22', 'aaa@aaa', '4124bc0a9335c27f086f24ba207a4912', 1);
+(1, 'ee', 'ee', '2018-06-08', 'e@e', '08a4415e9d594ff960030b921d42b91e', 1),
+(2, 'p', 'p', '2018-06-08', 'po@po', '83878c91171338902e0fe0fb97a8c47a', 2);
 
 --
 -- Indexes for dumped tables
@@ -217,49 +158,47 @@ INSERT INTO `usuario` (`idUsuario`, `nome`, `cpf`, `data`, `email`, `senha`, `ti
 -- Indexes for table `atividade`
 --
 ALTER TABLE `atividade`
-  ADD PRIMARY KEY (`idAtividade`,`idConjuntoAtividade`);
+  ADD PRIMARY KEY (`idAtividade`),
+  ADD KEY `fk_atividade_conjunto_atividade1_idx` (`idConjuntoAtividade`);
 
 --
 -- Indexes for table `conjunto_atividade`
 --
 ALTER TABLE `conjunto_atividade`
-  ADD PRIMARY KEY (`idConjuntoAtividade`);
+  ADD PRIMARY KEY (`idConjuntoAtividade`),
+  ADD KEY `fk_conjunto_atividade_Usuario1_idx` (`idProfessor`),
+  ADD KEY `fk_conjunto_atividade_disciplina1_idx` (`idDisciplina`);
 
 --
 -- Indexes for table `disciplina`
 --
 ALTER TABLE `disciplina`
-  ADD PRIMARY KEY (`idDisciplina`,`idProfessor`) USING BTREE;
+  ADD PRIMARY KEY (`idDisciplina`),
+  ADD KEY `fk_disciplina_Usuario_idx` (`idProfessor`);
 
 --
 -- Indexes for table `participa_disciplina`
 --
 ALTER TABLE `participa_disciplina`
-  ADD PRIMARY KEY (`idAluno`,`idDisciplina`);
+  ADD PRIMARY KEY (`idAluno`,`idDisciplina`),
+  ADD KEY `fk_Usuario_has_disciplina_disciplina1_idx` (`idDisciplina`),
+  ADD KEY `fk_Usuario_has_disciplina_Usuario1_idx` (`idAluno`);
 
 --
 -- Indexes for table `realiza_atividade`
 --
 ALTER TABLE `realiza_atividade`
-  ADD PRIMARY KEY (`idAtividade`,`idAluno`) USING BTREE;
+  ADD PRIMARY KEY (`idAluno`,`idAtividade`),
+  ADD KEY `fk_Usuario_has_atividade_atividade1_idx` (`idAtividade`),
+  ADD KEY `fk_Usuario_has_atividade_Usuario1_idx` (`idAluno`);
 
 --
--- Indexes for table `solicitacoes_disciplinas`
+-- Indexes for table `solicitacao_disciplina`
 --
-ALTER TABLE `solicitacoes_disciplinas`
-  ADD PRIMARY KEY (`idSolicitacao`);
-
---
--- Indexes for table `trofeus`
---
-ALTER TABLE `trofeus`
-  ADD PRIMARY KEY (`idTrofeu`);
-
---
--- Indexes for table `trofeus_atividades`
---
-ALTER TABLE `trofeus_atividades`
-  ADD PRIMARY KEY (`idAtividade`,`idTrofeu`);
+ALTER TABLE `solicitacao_disciplina`
+  ADD PRIMARY KEY (`idAluno`,`idDisciplina`,`idSolicitacao`),
+  ADD KEY `fk_Usuario_has_disciplina_disciplina2_idx` (`idDisciplina`),
+  ADD KEY `fk_Usuario_has_disciplina_Usuario2_idx` (`idAluno`);
 
 --
 -- Indexes for table `usuario`
@@ -275,37 +214,69 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT for table `atividade`
 --
 ALTER TABLE `atividade`
-  MODIFY `idAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `idAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `conjunto_atividade`
 --
 ALTER TABLE `conjunto_atividade`
-  MODIFY `idConjuntoAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `idConjuntoAtividade` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `disciplina`
 --
 ALTER TABLE `disciplina`
-  MODIFY `idDisciplina` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- AUTO_INCREMENT for table `solicitacoes_disciplinas`
---
-ALTER TABLE `solicitacoes_disciplinas`
-  MODIFY `idSolicitacao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `trofeus`
---
-ALTER TABLE `trofeus`
-  MODIFY `idTrofeu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idDisciplina` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `atividade`
+--
+ALTER TABLE `atividade`
+  ADD CONSTRAINT `fk_atividade_conjunto_atividade` FOREIGN KEY (`idConjuntoAtividade`) REFERENCES `conjunto_atividade` (`idConjuntoAtividade`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limitadores para a tabela `conjunto_atividade`
+--
+ALTER TABLE `conjunto_atividade`
+  ADD CONSTRAINT `fk_conjunto_atividade_da_disciplina` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`idDisciplina`),
+  ADD CONSTRAINT `fk_conjunto_atividade_professor` FOREIGN KEY (`idProfessor`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Limitadores para a tabela `disciplina`
+--
+ALTER TABLE `disciplina`
+  ADD CONSTRAINT `fk_disciplina_Usuario` FOREIGN KEY (`idProfessor`) REFERENCES `usuario` (`idUsuario`);
+
+--
+-- Limitadores para a tabela `participa_disciplina`
+--
+ALTER TABLE `participa_disciplina`
+  ADD CONSTRAINT `fk_Usuario_participa` FOREIGN KEY (`idAluno`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `fk_disciplina_participa` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`idDisciplina`);
+
+--
+-- Limitadores para a tabela `realiza_atividade`
+--
+ALTER TABLE `realiza_atividade`
+  ADD CONSTRAINT `fk_aluno_realiza_atividade` FOREIGN KEY (`idAluno`) REFERENCES `usuario` (`idUsuario`),
+  ADD CONSTRAINT `fk_atividade_realizada` FOREIGN KEY (`idAtividade`) REFERENCES `atividade` (`idAtividade`);
+
+--
+-- Limitadores para a tabela `solicitacao_disciplina`
+--
+ALTER TABLE `solicitacao_disciplina`
+  ADD CONSTRAINT `fk_Usuario_disciplina_solicitada` FOREIGN KEY (`idAluno`) REFERENCES `usuario` (`idUsuario`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_Usuario_solicita_disciplina` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`idDisciplina`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
